7ac7612f7d50873dc185aed5a44ed2881b35181e
rodri1110 <rodri.oliveira1110@gmail.com>
2022-02-03 08:28:34 -0300

    Interpolação, diretiva e binding

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -7,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./eventos.component.scss']
 })
 export class EventosComponent implements OnInit {
-
+  eventosFiltrados: any =[];
   public eventos: any;
+  mostrar = true;
+  private _filtroLista: string = '';
 
   constructor(private http: HttpClient) { }
+
+  public get filtroLista(): string{
+    return this._filtroLista;
+  }
+
+  public set filtroLista(value: string){
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+  }
+
+  filtrarEventos(filtrarPor: string): any{
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento: {tema: string; local: string;}) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+      evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    )
+  }
 
   ngOnInit() {
     this.getEventos();
@@ -18,7 +37,10 @@ export class EventosComponent implements OnInit {
 
   public getEventos(): void {
     this.http.get('https://localhost:5001/api/evento').subscribe(
-      response => this.eventos = response,
+      response => {
+        this.eventosFiltrados = response
+        this.eventos = response
+      },
       error => console.log(error)
       );
   }
